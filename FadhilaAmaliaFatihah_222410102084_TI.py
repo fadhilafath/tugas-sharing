4908ab937bf8e7611b632395857d49d442ffba4e
fadhilafath <117618073+fadhilafath@users.noreply.github.com>
2022-11-07 10:49:57 +0700

    Add files via upload

---
+++ FadhilaAmaliaFatihah_222410102084_TI.py
@@ -0,0 +1,19 @@
+nama = input("masukkan nama:")
+nim = input("masukkan nim:")
+tempattanggallahir = input("masukkan tempat tanggal lahir:")
+umur = input("masukkan umur:")
+jeniskelamin = input("masukkan jenis kelamin:")
+jurusan = input("masukkan jurusan:")
+alamat = input("masukkan alamat:")
+
+
+print (nama)
+print (nim)
+nim = int(nim)
+print (tempattanggallahir)
+print (umur)
+umur = int(umur)
+print (jeniskelamin)
+print (jurusan)
+print (alamat)
+
